9c3934196f0b8be085e7acdf6e7f2bcafc11434c
Your Name <you@example.com>
2022-12-07 12:57:09 +0530

    Added: refactor code to take mood message in constructor and return happy

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -8,9 +8,10 @@ public class MoodAnalyserTest {
 
     public void givenMessage_WhenProper_ShouldReturnSad() {
 
-        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad mood");
-        String sadMood = moodAnalyser.analyseMood();
-        System.out.println(sadMood);
-        Assertions.assertEquals("SAD", sadMood);
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy mood");
+        String happyMood = moodAnalyser.analyseMood();
+        System.out.println(happyMood);
+        Assertions.assertEquals("HAPPY", happyMood);
+
     }
 }
